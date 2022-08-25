35f8132ab70237e5fdcd3fd2b8bfdc2b1be5d268
John-Kimani <kimwaweru100@gmail.com>
2022-08-26 01:28:09 +0300

    Link  login and signup page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React , {useRef, useState} from 'react'
+import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from '../context/AuthContext'
+import { Link } from 'react-router-dom'
 
 export default function Login() {
+    //set local referance to useRef lib
+    const emailRef = useRef()
+    const passwordRef = useRef()
+    // Points signup Function directly to sign up context
+    const { signup } = useAuth()
+    // Error handler using use state hook
+    const [error, setError] = useState('')
+    // Create loading state
+    const [ loading, setLoading ] = useState(false)
+
+    async function handleSubmit(e){
+        e.preventDefault()
+        // Prevents form from refreshing
+
+
+        try {
+            //set error back to empty string
+            setError('')
+            setLoading(true)
+            await  signup(emailRef.current.value, passwordRef.current.value)
+        }catch{
+            setError('Failed to create an account')
+        }
+
+        // Set loading status to false after clicking on submit
+        setLoading(false)
+    }
+
+
   return (
-    <div>Login</div>
+    <>
+    <Card>
+        <Card.Body>
+            <h2 className='text-center mb-4'>Log In</h2>
+            { error && <Alert variant='danger'>{error}</Alert>}
+            <Form onSubmit={handleSubmit}>
+                <Form.Group id="email">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="email" ref={emailRef} required></Form.Control>
+                </Form.Group>
+
+                <Form.Group id="password">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" ref={passwordRef} required></Form.Control>
+                </Form.Group>
+
+                <Button disabled={loading} className='w-100 mt-2' type="submit">Log In</Button>
+            </Form>
+        </Card.Body>
+    </Card>
+    <div className='w-100 text-center mt-2'>
+        Need an account? <Link to="/signup">Sign Up</Link>
+    </div>
+    </>
   )
 }
