869cfec18b6c6bfc78acb44c8ea024d3a3e00a53
John-Kimani <kimwaweru100@gmail.com>
2022-08-26 01:24:06 +0300

    Rout homepage and login component

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,8 @@ import Signup from "./Signup";
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "../context/AuthContext"
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
+import HomePage from "./HomePage";
+import Login from "./Login";
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
             <Routes>
             <Route exact path="/" element={<HomePage/>}/>
               <Route path='/signup' element={<Signup/>} />
+              <Route path='/login' element={<Login/>} />
             </Routes>
           </AuthProvider>
         </Router>
