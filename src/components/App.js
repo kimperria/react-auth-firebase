1ad3735c5225415619aadc97a644ee90e3162432
John-Kimani <kimwaweru100@gmail.com>
2022-08-26 01:21:23 +0300

    Route: Signup page and homepage to exact path /

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,25 @@
 import Signup from "./Signup";
 import { Container } from 'react-bootstrap'
 import { AuthProvider } from "../context/AuthContext"
+import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom"
 
 function App() {
   return (
-    <AuthProvider>
-
-
     <Container 
     className="d-flex align-items-center justify-content-center"
     style={{ minHeight: "100vh"}}>
       <div className="w-100" style={{ maxWidth: '400px'}}>
-      <Signup />
+        <Router>
+          <AuthProvider>
+            <Routes>
+            <Route exact path="/" element={<HomePage/>}/>
+              <Route path='/signup' element={<Signup/>} />
+            </Routes>
+          </AuthProvider>
+        </Router>
       </div>
     </Container>
-    </AuthProvider>
+
     )
 }
 
