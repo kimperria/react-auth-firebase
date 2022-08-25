85d03363f749e6d533930047270e49ec88f6ddc6
John-Kimani <kimwaweru100@gmail.com>
2022-08-26 00:10:22 +0300

    Set up Auth State Changed method to enable signup

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { auth } from '../firebase'
 
 //create auth context
 const AuthContext = React.createContext()
@@ -11,10 +12,29 @@ export function useAuth(){
 export function AuthProvider({children}) {
     // user state to handle currentUser
     const [ currentUser, setCurrentUser ] = useState()
+    
+    //signup function with uses email and password arguement to return a promise
+    function signup(email, password) {
+       return auth.createUserWithEmailAndPassword(email, password)
+    }
+
+    //useEffect to avoid setUser method to run on render [ ] ensure it runs only one
+    useEffect(() =>{
+    //method backed with firebase to set user
+        const unsubscribe = auth.onAuthStateChanged(user => {
+            // this function return a method to unsubscribe on Auth State Chnage
+            setCurrentUser(user)
+        })
+
+        return unsubscribe
+        // unsubscribe from event listener
+    }, [])
+
 
     const value = {
         // infomation provided with authentication
-        currentuser
+        currentUser,
+        signup
     }
 
   return (
